d536a0fb65f8f379048aa1d2ca3fdcb6d036c7c2
Prachiti Vaidya <146661146+prachitivaidya@users.noreply.github.com>
2024-05-30 16:43:36 +0530

    Add files via upload

---
+++ day16/main.py
@@ -0,0 +1,35 @@
+# from prettytable import PrettyTable
+# table = PrettyTable()
+#
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = "l"
+#
+# print(table)
+
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+money_machine.report()
+coffee_maker.report()
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
